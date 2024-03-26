e46ee5c01e295f58e0369da61a88222e45f02584
Mahelet,Kassa <maheletkassa1@gmail.com>
2024-03-26 14:24:49 -0400

    INIT

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+  return null
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ZEESHU</div>
+          <div className="right">
+            <p>Z6 Gulshan-e-Maymar, Karachi</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By CODEWITHZEESHU</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By CodeWithZeeshu.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
